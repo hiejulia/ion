4247a033816fcce67aecbb99ed376207409a5711
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-15 21:03:29 +0200

    sign up page

--- project/src/pages/login/login.ts
+++ project/src/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,LoadingController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 //import { UserData } from '../../providers/user-data';
 import { TabsPage } from '../tabs/tabs';
 import { SignupPage } from '../signup/signup';
 import { AuthProvider } from '../../providers/auth';
+import {TechEvents} from '../techevents/techevents';
 
 
 
@@ -24,9 +25,57 @@ export class LoginPage {
   password:string;
   loading:any;
 
-  constructor(public navCtrl: NavController, public authService: AuthProvider) {
+  constructor(public navCtrl: NavController, public authService: AuthProvider,public loadingCtrl: LoadingController) {
  
     }
-
-
+ionViewDidLoad() {
+ 
+        this.showLoader();
+ 
+        //Check if already authenticated
+        this.authService.checkAuthentication().then((res) => {
+            console.log("Already authorized");
+            this.loading.dismiss();
+            this.navCtrl.setRoot(TechEvents);
+        }, (err) => {
+            console.log("Not already authorized");
+            this.loading.dismiss();
+        });
+ 
+    }
+ 
+    login(){
+ 
+        this.showLoader();
+ 
+        let credentials = {
+            email: this.email,
+            password: this.password
+        };
+ 
+        this.authService.login(credentials).then((result) => {
+            this.loading.dismiss();
+            console.log(result);
+            this.navCtrl.setRoot(TechEvents);
+        }, (err) => {
+            this.loading.dismiss();
+            console.log(err);
+        });
+ 
+    }
+ 
+    launchSignup(){
+        this.navCtrl.push(SignupPage);
+    }
+ 
+    showLoader(){
+ 
+        this.loading = this.loadingCtrl.create({
+            content: 'Authenticating...'
+        });
+ 
+        this.loading.present();
+ 
+    }
+ 
 }
