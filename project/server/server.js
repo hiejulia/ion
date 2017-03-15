181870442785275413011329d805b4b8060f63ff
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-15 19:17:46 +0200

    update server.js main entry for server :fire:

---
+++ project/server/server.js
@@ -0,0 +1,25 @@
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var databaseConfig = require('./config/database');
+var router = require('./app/routes');
+
+var port = process.env.PORT || 3000;
+//connect to mongodb
+mongoose.connnect(databaseConfig.url);
+
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json()); // Send JSON responses
+app.use(logger('dev')); // Log requests to API using morgan
+app.use(cors());
+ 
+router(app);
+
+
+app.listen(port);
+consolelog('Running on port '+port);
+
