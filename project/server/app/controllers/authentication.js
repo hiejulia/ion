a0e6d70bf5478d1e99ef038b8142b9d17f749260
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-18 22:15:57 +0300

    update controller in the back end

--- project/server/app/controllers/authentication.js
+++ project/server/app/controllers/authentication.js
@@ -1,103 +1,41 @@
-var jwt = require('jsonwebtoken');  
-var User = require('../models/user');
-var authConfig = require('../../config/auth');
- 
-function generateToken(user){
-    return jwt.sign(user, authConfig.secret, {
-        expiresIn: 10080
-    });
-}
- 
-function setUserInfo(request){
-    return {
-        _id: request._id,
-        email: request.email,
-        role: request.role
-    };
-}
- 
-exports.login = function(req, res, next){
- 
-    var userInfo = setUserInfo(req.user);
- 
-    res.status(200).json({
-        token: 'JWT ' + generateToken(userInfo),
-        user: userInfo
-    });
- 
-}
- 
-exports.register = function(req, res, next){
- 
-    var email = req.body.email;
-    var password = req.body.password;
-    var role = req.body.role;
- 
-    if(!email){
-        return res.status(422).send({error: 'You must enter an email address'});
+'use strict';
+
+/**
+ *  Module dependencies
+ */
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+/**
+ *  Module exports
+ */
+module.exports.signin = signinUser;
+module.exports.signout = signoutUser;
+
+/**
+ *  Uses Passport's local strategy to sign in a user
+ */
+function signinUser(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
     }
- 
-    if(!password){
-        return res.status(422).send({error: 'You must enter a password'});
+
+    if (!user) {
+      return res.status(400).json(info);
     }
- 
-    User.findOne({email: email}, function(err, existingUser){
- 
-        if(err){
-            return next(err);
-        }
- 
-        if(existingUser){
-            return res.status(422).send({error: 'That email address is already in use'});
-        }
- 
-        var user = new User({
-            email: email,
-            password: password,
-            role: role
-        });
- 
-        user.save(function(err, user){
- 
-            if(err){
-                return next(err);
-            }
- 
-            var userInfo = setUserInfo(user);
- 
-            res.status(201).json({
-                token: 'JWT ' + generateToken(userInfo),
-                user: userInfo
-            })
- 
-        });
- 
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(200).json(user);
     });
- 
+  })(req, res, next);
 }
- 
-exports.roleAuthorization = function(roles){
- 
-    return function(req, res, next){
- 
-        var user = req.user;
- 
-        User.findById(user._id, function(err, foundUser){
- 
-            if(err){
-                res.status(422).json({error: 'No user found.'});
-                return next(err);
-            }
- 
-            if(roles.indexOf(foundUser.role) > -1){
-                return next();
-            }
- 
-            res.status(401).json({error: 'You are not authorized to view this content'});
-            return next('Unauthorized');
- 
-        });
- 
-    }
- 
+
+function signoutUser(req, res, next) {
+  req.logout();
+  res.redirect('/');
 }
--- project/server/config/models.js
+++ project/server/config/models.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+module.exports.init = initModels;
+
+function initModels(app) {
+  const modelsPath = app.get('root') + '/app/models/';
+
+  ['profile-block', 'user', 'company', 'job', 'application'].forEach(function(model) {
+    require(modelsPath + model);
+  });
+};
