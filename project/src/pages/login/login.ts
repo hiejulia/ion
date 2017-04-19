e13c32bec27b39cc78a2dd9f00d93c685c52e316
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-19 21:59:28 +0300

    fix bugs in the login page

--- project/src/pages/login/login.ts
+++ project/src/pages/login/login.ts
@@ -30,6 +30,17 @@ export class LoginPage {
       
     }
 ionViewDidLoad() {
+   this.showLoader();
+ 
+        //Check if already authenticated
+        this.authService.checkAuthentication().then((res) => {
+            console.log("Already authorized");
+            this.loading.dismiss();
+            this.navCtrl.setRoot(HomePage);
+        }, (err) => {
+            console.log("Not already authorized");
+            this.loading.dismiss();
+        });
  
        
  
@@ -37,11 +48,39 @@ ionViewDidLoad() {
 
  
    
+login(){
+ 
+        this.showLoader();
+ 
+        let credentials = {
+            email: this.email,
+            password: this.password
+        };
+ 
+        this.authService.login(credentials).then((result) => {
+            this.loading.dismiss();
+            console.log(result);
+            this.navCtrl.setRoot(HomePage);
+        }, (err) => {
+            this.loading.dismiss();
+            console.log(err);
+        });
 
+}
   
 
-    createAccount(){
-      this.navCtrl.push(RegisterPage);
+   launchSignup(){
+        this.navCtrl.push(RegisterPage);
+    }
+ 
+    showLoader(){
+ 
+        this.loading = this.loadingCtrl.create({
+            content: 'Authenticating...'
+        });
+ 
+        this.loading.present();
+ 
     }
  
 }
