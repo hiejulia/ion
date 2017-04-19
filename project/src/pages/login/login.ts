8f33cdd899a0f3ceb3d1f90f14df6c329ebc11a1
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-19 21:58:10 +0300

    updating..

--- project/src/pages/login/login.ts
+++ project/src/pages/login/login.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams,LoadingController } from 'ionic-angular';
-// import { NgForm } from '@angular/forms';
-// //import { UserData } from '../../providers/user-data';
-// import { TabsPage } from '../tabs/tabs';
-// import { AuthProvider } from '../../providers/auth';
-// import {TechEvents} from '../techevents/techevents';
+
 import {RegisterPage} from '../register/register';
 import {TabsPage} from '../tabs/tabs';
 
-import { AuthService } from '../../providers/auth-service';
+
+import { Auth } from '../../providers/auth';
+import { HomePage } from '../home/home';
 
 
 
@@ -23,78 +21,22 @@ import { AuthService } from '../../providers/auth-service';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  // email:string;
-  // password:string;
-  // loading:any;
-private _authService: AuthService;
+  email: string;
+    password: string;
+    loading: any;
 
-  private _navCtrl: NavController;
-  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController,authService: AuthService) {
-      this._authService = authService;
-    this._navCtrl = navCtrl;
- 
+  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController,
+  public authService: Auth) {
+      
     }
 ionViewDidLoad() {
  
-        // this.showLoader();
- 
-        // //Check if already authenticated
-        // this.authService.checkAuthentication().then((res) => {
-        //     console.log("Already authorized");
-        //     this.loading.dismiss();
-        //     this.navCtrl.setRoot(TechEvents);
-        // }, (err) => {
-        //     console.log("Not already authorized");
-        //     this.loading.dismiss();
-        // });
+       
  
     }
-    signin(event, email, password) {
-    event.preventDefault();
 
-    let data = { email, password };
-
-    this._authService
-    .signin(data)
-    .subscribe((user) => {
-      this.navCtrl.push(TabsPage);
-    }, err => console.error(err));
-    this.navCtrl.push(TabsPage);
-  }
  
-    // login(){
- 
-    //     this.showLoader();
- 
-    //     let credentials = {
-    //         email: this.email,
-    //         password: this.password
-    //     };
- 
-    //     this.authService.login(credentials).then((result) => {
-    //         this.loading.dismiss();
-    //         console.log(result);
-    //         this.navCtrl.setRoot(TechEvents);
-    //     }, (err) => {
-    //         this.loading.dismiss();
-    //         console.log(err);
-    //     });
- 
-    // }
- 
-    // launchSignup(){
-    //     this.navCtrl.push(SignupPage);
-    // }
- 
-    // showLoader(){
- 
-    //     this.loading = this.loadingCtrl.create({
-    //         content: 'Authenticating...'
-    //     });
- 
-    //     this.loading.present();
- 
-    // }
+   
 
   
 
--- project/src/providers/auth.ts
+++ project/src/providers/auth.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { Http, Headers } from '@angular/http';
+import { Storage } from '@ionic/storage';
 /*
   Generated class for the Auth provider.
 
@@ -10,9 +10,87 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class Auth {
+  public token: any;
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public storage: Storage) {
     console.log('Hello Auth Provider');
+  }
+
+
+  checkAuthentication(){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        //Load token if exists
+        this.storage.get('token').then((value) => {
+ 
+            this.token = value;
+ 
+            let headers = new Headers();
+            headers.append('Authorization', this.token);
+ 
+            this.http.get('http://localhost:8080/api/auth/protected', {headers: headers})
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => {
+                    reject(err);
+                }); 
+ 
+        });         
+ 
+    });
+ 
+  }
+ 
+  createAccount(details){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+ 
+        this.http.post('http://localhost:8080/api/auth/register', JSON.stringify(details), {headers: headers})
+          .subscribe(res => {
+ 
+            let data = res.json();
+            this.token = data.token;
+            this.storage.set('token', data.token);
+            resolve(data);
+ 
+          }, (err) => {
+            reject(err);
+          });
+ 
+    });
+ 
+  }
+ 
+  login(credentials){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+ 
+        this.http.post('http://localhost:8080/api/auth/login', JSON.stringify(credentials), {headers: headers})
+          .subscribe(res => {
+ 
+            let data = res.json();
+            this.token = data.token;
+            this.storage.set('token', data.token);
+            resolve(data);
+ 
+            resolve(res.json());
+          }, (err) => {
+            reject(err);
+          });
+ 
+    });
+ 
+  }
+ 
+  logout(){
+    this.storage.set('token', '');
   }
 
 }
