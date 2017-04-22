f11728da959860b99ab9d1f7ba1af2f7c0a5f91c
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-22 18:33:15 +0300

    updating with map

--- project/src/pages/geolocation/geolocation.ts
+++ project/src/pages/geolocation/geolocation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker, CameraPosition } from 'ionic-native';
 
 /*
   Generated class for the Geolocation page.
@@ -12,11 +13,56 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'geolocation.html'
 })
 export class GeolocationPage {
+  public map: GoogleMap;
+  public mapRendered: Boolean = false; 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.showMap();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GeolocationPage');
+  }
+
+  showMap(){
+    let location = new GoogleMapsLatLng(47.6062, -122.3321);
+    this.map = new GoogleMap('map', {
+      'camera': {
+        'latLng': location,
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(()=> {
+      console.log('Map is ready!');
+      this.mapRendered = true;
+    });
+  }
+getMyLocation() {
+    this.map.getMyLocation().then((location) => {
+      var msg = ["I am here:\n",
+        "latitude:" + location.latLng.lat,
+        "longitude:" + location.latLng.lng].join("\n");
+
+      let position: CameraPosition = {
+        target: location.latLng,
+        zoom: 15
+      };
+      this.map.moveCamera(position);    
+
+      let markerOptions: GoogleMapsMarkerOptions = {
+        'position': location.latLng,
+        'title': msg
+      };
+      this.map.addMarker(markerOptions).then((marker: GoogleMapsMarker) => {
+        marker.showInfoWindow();
+      });
+
+    });
+
   }
 
 }
