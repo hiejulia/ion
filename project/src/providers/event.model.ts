41a8fc80f7d10f3ed74564560d3c3562d1621da3
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-26 00:59:16 +0300

    finish event model

---
+++ project/src/providers/event.model.ts
@@ -0,0 +1,55 @@
+export class EventModel {
+  _id: string;
+  title: string;
+  slug: string;
+  location:string;
+  description: string;
+  office:string;
+  address:string;
+  typeOfEvent: string;
+  numberOfParticipantsEstimated:number;
+  isActive:boolean;
+  timeStart:string;
+  timeEnd:string;
+  organisation: string;
+  industry: string;
+  country: string;
+  createdAt: string;
+  userCreated:string;
+
+  constructor(
+    _id?: string,
+    title?: string,
+    slug?: string,
+    location?:string,
+    description?: string,
+    office?:string;
+  address?:string;
+  typeOfEvent?: string;
+  numberOfParticipantsEstimated?:number;
+  isActive?:boolean;
+  timeStart?:string;
+  timeEnd?:string;
+  organisation?: string;
+  industry?: string;
+  country?: string;
+  createdAt?: string;
+  ) {
+    this._id = _id;
+    this.title = title;
+    this.slug = slug;
+    this.location = location;
+    this.office = office;
+    this.address= address;
+    this.description = description;
+    this.typeOfEvent = typeOfEvent;
+    this.numberOfParticipantsEstimated = numberOfParticipantsEstimated;
+    this.isActive = isActive;
+    this.timeStart= timeStart;
+    this.timeEnd = timeEnd;
+    this.organisation = organisation;
+    this.industry = industry;
+    this.country = country;
+    this.createdAt = createdAt;
+  }
+}
