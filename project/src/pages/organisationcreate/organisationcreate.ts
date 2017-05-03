4fb7103d6164330cf143d27a43901fc0c212f419
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-04 00:54:31 +0300

    updating...final folder

--- project/src/pages/organisationcreate/organisationcreate.ts
+++ project/src/pages/organisationcreate/organisationcreate.ts
@@ -1,83 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { ViewController } from 'ionic-angular';
-import { OrganisationServiceProvider } from '../../providers/organisationService';
-import { OrganisationModel } from '../../providers/organisation.model';
-import {  LoadingController,ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {ViewController} from 'ionic-angular';
+import {OrganisationServiceProvider} from '../../providers/organisationService';
+import {OrganisationModel} from '../../providers/organisation.model';
+import {LoadingController, ToastController} from 'ionic-angular';
 
-@Component({
-  selector: 'OrganisationCreate',
-  templateUrl: 'organisationcreate.html'
-})
+@Component({selector: 'OrganisationCreate', templateUrl: 'organisationcreate.html'})
 export class OrganisationCreatePage {
-  public organisation: OrganisationModel;
-  private _organisationServiceProvider: OrganisationServiceProvider;
+  public organisation : OrganisationModel;
+  private _organisationServiceProvider : OrganisationServiceProvider;
 
-  name:any;
-  location:any;
-  description:any;
-  country:any;
-  address:any;
-  numberOfEmployees:any;
-  industry:any;
- 
-  constructor(public viewCtrl: ViewController, organisationServiceProvider:OrganisationServiceProvider,
-  private toastCtrl: ToastController) {
-      this._organisationServiceProvider = organisationServiceProvider;
-      this.organisation= new OrganisationModel();
+  name : any;
+  location : any;
+  description : any;
+  country : any;
+  address : any;
+  numberOfEmployees : any;
+  industry : any;
 
-      
- 
+  constructor(public viewCtrl : ViewController, organisationServiceProvider : OrganisationServiceProvider, private toastCtrl : ToastController) {
+    this._organisationServiceProvider = organisationServiceProvider;
+    this.organisation = new OrganisationModel();
+
   }
- 
 
+  save() : void {
 
- 
-save(): void {
- 
     let organisation = {
-        name:this.name,
-        location:this.location,
-        description:this.description,
-        country:this.country,
-        address:this.address,
-        numberOfEmployees:this.numberOfEmployees,
-        industry:this.industry,
-        owner:localStorage.getItem('user_Id')
-      
+      name: this.name,
+      location: this.location,
+      description: this.description,
+      country: this.country,
+      address: this.address,
+      numberOfEmployees: this.numberOfEmployees,
+      industry: this.industry,
+      owner: localStorage.getItem('user_Id')
+
     };
- 
-    this.viewCtrl.dismiss(organisation);
+
+    this
+      .viewCtrl
+      .dismiss(organisation);
     console.log(organisation);
-   let toast = this.toastCtrl.create({
-    message: 'Organisation added successfully',
-    duration: 2000,
-    position: 'bottom'
-  });
+    let toast = this
+      .toastCtrl
+      .create({message: 'Organisation added successfully', duration: 2000, position: 'bottom'});
 
-  toast.onDidDismiss(() => {
-    console.log('Dismissed toast');
-  });
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
 
-  toast.present();
-    
- 
-  } 
+    toast.present();
 
+  }
 
-  
-close():void {
-    this.viewCtrl.dismiss();
- 
+  close() : void {
+    this
+      .viewCtrl
+      .dismiss();
+
+  }
 
 }
-
-}
-
-
-
-
-
-
-
-
