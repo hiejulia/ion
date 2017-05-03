1f6a18e388ff260bd89a46b58b0355a1ab89ead6
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-04 01:55:05 +0300

    fix the back end

--- project/server2/config/environments/development.js
+++ project/server2/config/environments/development.js
@@ -30,12 +30,7 @@ module.exports = {
 		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
 		callbackURL: '/auth/facebook/callback'
 	},
-	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-		callbackURL: '/auth/twitter/callback'
-	},
-
+	
 	mailer: {
 		from: process.env.MAILER_FROM || 'MAILER_FROM',
 		options: {
